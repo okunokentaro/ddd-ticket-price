0b2069841d79f390128bb44145b2cc78ea848d09
OKUNOKENTARO <okunokentaro@me.com>
2019-07-15 23:44:10 +0900

    cosme

--- main.ts
+++ main.ts
@@ -1,9 +1,9 @@
 import { nationalHolidayDefinitions } from './src/lib/national-holiday.definitions';
-import { calcPricesFactory } from './src/models/price/calc-prices-factory';
 import { Customer } from './src/models/customer/customer';
 import { Movie } from './src/models/movie/movie';
 import { periodDefinitions } from './src/models/period/definitions/period.definitions';
 import { ViewModel } from './src/models/presentation/view-model';
+import { calcPricesFactory } from './src/models/price/calc-prices-factory';
 
 function main(origin: Date) {
   const calcPrices = calcPricesFactory(
