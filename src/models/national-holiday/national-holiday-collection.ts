2a5a71b9e65eb9daf62e6d48311da1c341674f13
OKUNOKENTARO <okunokentaro@me.com>
2019-07-15 23:40:29 +0900

    refactor withinNationalHoliday()

--- src/models/national-holiday/national-holiday-collection.ts
+++ src/models/national-holiday/national-holiday-collection.ts
@@ -4,8 +4,11 @@ export class NationalHolidayCollection {
   constructor(private readonly list: NationalHoliday[]) {}
 
   withinNationalHoliday(unixTime: number): boolean {
-    return this.list.some(v => {
-      return v.time <= unixTime && unixTime < v.time + 24 * 60 * 60;
+    return this.list.some(holiday => {
+      return [
+        holiday.time <= unixTime,
+        unixTime < holiday.time + 24 * 60 * 60,
+      ].every(v => v);
     });
   }
 }
